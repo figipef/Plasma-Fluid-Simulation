4175439c66b441eaa9ce2d4ed2d7f101700395e7
André <andre.g.filipe@gmail.com>
2025-02-17 14:50:25 +0000

    Changed Chemistry class and started defining an input file to simplify program usage

--- inc/chemistry.hpp
+++ inc/chemistry.hpp
@@ -1,11 +1,22 @@
 #pragma once
 
+#include <fstream>
+#include <iostream>
+
 class chemistry{
 
 	public:
 
-		chemistry();
+		chemistry(int n_species, int n_reactions, );
 
-		~chemistry();
+		double calc_reaction_rate();
+
+		double setup_species_react_net(std::ofstream&);
+
+	private:
+
+		double *reaction_rates; // For each reaction (i)
+
+		int **species_react_net; // For each species (i) save the net gain/loss for each reaction (j)
 
 }
